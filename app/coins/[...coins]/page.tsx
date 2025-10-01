9593c16e6a8450df1fcaf8a942797dfda38a212b
not-a-ethan <92686703+not-a-ethan@users.noreply.github.com>
2025-10-01 09:00:12 -0400

    Added cases for incorrect amount of dynamic routes

--- app/coins/[...coins]/page.tsx
+++ app/coins/[...coins]/page.tsx
@@ -1,10 +1,27 @@
 import { headers } from "next/headers";
+import { redirect, RedirectType } from 'next/navigation'
 
 export default async function CoinEntry({ params }: { params: Promise<{ coins: string[] }> }) {
     const headersList = await headers();
     const domain = headersList.get("host");
 
     const { coins } = await params;
+
+    // Makes sure nothing past 2 | Ex: "doamin/coins/levelOne/levelTwo"
+    if (coins.length > 2) {
+        redirect(`http://${domain}/coins/${coins[0]}/${coins[1]}`, RedirectType.push);
+    };
+
+    // Coin type pages | Ex: pennies
+    if (coins.length == 1) {
+        return (
+            <>
+                <h1>This is a coin type page!</h1>
+            </>
+        )
+    };
+
+    // For specific coin | Ex: Shield penny
 
     const res: any = await fetch(`http://${domain}/api/coins?category=${coins[0]}&entry=${coins[1]}`);
     const json = await res.json();
@@ -13,5 +30,5 @@ export default async function CoinEntry({ params }: { params: Promise<{ coins: s
         <>
             <span dangerouslySetInnerHTML={{ __html: json["html"]}}></span>
         </>
-    )
-} 
+    );
+} ;
